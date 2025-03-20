5e802cedbeb86cea65422097d0b8feca58e90773
Adrian Maj <adrianmaj1122@gmail.com>
2025-03-20 13:25:58 +0100

    fix: exported client feature

--- src/client.ts
+++ src/client.ts
@@ -4,3 +4,4 @@ export { TextColorClientFeature } from "./features/textColor/feature.client";
 export { TextSizeClientFeature } from "./features/textSize/feature.client";
 export { TextLetterSpacingClientFeature } from "./features/textLetterSpacing/feature.client";
 export { TextLineHeightClientFeature } from "./features/textLineHeight/feature.client";
+export { TextClassClientFeature } from "./features/textClass/feature.client";
